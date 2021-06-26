8d61a01d3094bfd497bd230120cbbdda8faf2256
Jason Diaz Jimenez <86149507+VulpoTheDev@users.noreply.github.com>
2021-06-26 11:15:01 -0500

    Update sshkeysGithub.sh

--- sshkeysGithub.sh
+++ sshkeysGithub.sh
@@ -2,4 +2,5 @@
 
 read -p "Enter your email: " email 
 ssh-keygen -t ed25519 -C "$email" -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
 echo  "Cool, Now add your SSH Key onto github https://github.com/settings/ssh/new"
