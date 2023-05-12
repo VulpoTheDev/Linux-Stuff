cbfc5450ab46641c27ef32fadc53fd30fe6659b6
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-05-13 00:09:33 +0800

    add y/n prompt to restart computer
    * also replaced `nodejs` with `nvm` to switch node versions with ease

--- installProgram.sh
+++ installProgram.sh
@@ -4,11 +4,18 @@ echo "Please make sure you're running this with root!"
 sudo pacman -Syu --noconfirm
 
 # Installs your Dev Stuff & Utils with pacman
-sudo pacman -S vim neovim nodejs npm base-devel obs-studio flameshot yay  jre8-openjdk-headless jre8-openjdk jdk8-openjdk openjdk8-doc openjdk8-src firefox-developer-edition go --noconfirm
+sudo pacman -S vim neovim base-devel obs-studio flameshot yay jre8-openjdk-headless jre8-openjdk jdk8-openjdk openjdk8-doc openjdk8-src firefox-developer-edition go --noconfirm
+
+# Install nvm and latest version of Node.js
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install stable
+nvm alias default stable
 
 # Clones and makepkg stuff
 git clone https://aur.archlinux.org/yay.git
-cd yay; makepkg -sic; cd ..
+cd yay
+makepkg -sic
+cd ..
 sudo rm -rd ./yay
 yay -Syu discord-canary mongodb-bin gitkraken mongodb-compass jetbrains-toolbox visual-studio-code-insiders-bin
 
@@ -20,4 +27,11 @@ sudo systemctl enable mongodb
 # Install Yarn and Global NPM Packages
 sudo npm i -g yarn typescript prettier eslint
 
-echo "Done, Now go restart your Computer!"
+while true; do
+  read -p "Done, restart computer? (y/n)" yn
+  case $yn in
+    [yY]* ) echo "Restarting computer"; shutdown -n now;;
+    [nN]* ) echo "Exiting"; exit;;
+    * ) echo "Please enter 'y' or 'n'!";;
+  esac
+done
