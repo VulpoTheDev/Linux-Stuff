aab333c3867422c880317c3d0d9487f975b4c86a
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-05-13 16:08:56 +0800

    place installable packages into array

--- installProgram.sh
+++ installProgram.sh
@@ -4,7 +4,25 @@ echo "Please make sure you're running this with root!"
 sudo pacman -Syu --noconfirm
 
 # Installs your Dev Stuff & Utils with pacman
-sudo pacman -S vim neovim base-devel obs-studio flameshot yay jre8-openjdk-headless jre8-openjdk jdk8-openjdk openjdk8-doc openjdk8-src firefox-developer-edition go python-pip python --noconfirm
+packages=(
+  vim
+  neovim
+  base-devel
+  obs-studio
+  flameshot
+  yay
+  jre8-openjdk-headless
+  jre8-openjdk
+  jdk8-openjdk
+  openjdk8-doc
+  openjdk8-src
+  firefox-developer-edition
+  go
+  python-pip
+  python
+)
+
+sudo pacman -S ${packages[@]} --noconfirm
 
 # Install nvm and latest version of Node.js
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
@@ -17,21 +35,43 @@ cd yay
 makepkg -sic
 cd ..
 sudo rm -rd ./yay
-yay -Syu discord-canary mongodb-bin gitkraken mongodb-compass jetbrains-toolbox visual-studio-code-insiders-bin
+
+yay_packages=(
+  discord-canary
+  mongodb-bin
+  gitkraken
+  mongodb-compass
+  jetbrains-toolbox
+  visual-studio-code-insiders-bin
+)
+
+yay -Syu ${yay_packages[@]}
 
 # Starts & Enables MongoDB
-
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
 
 # Install Yarn and Global NPM Packages
-sudo npm i -g yarn typescript prettier eslint
+npm_packages=(
+  yarn
+  typescript
+  prettier
+  eslint
+)
+
+sudo npm i -g ${npm_packages[@]}
 
 while true; do
   read -p "Done, restart computer? (y/n)" yn
   case $yn in
-    [yY]* ) echo "Restarting computer"; shutdown -n now;;
-    [nN]* ) echo "Exiting"; exit;;
-    * ) echo "Please enter 'y' or 'n'!";;
+  [yY]*)
+    echo "Restarting computer"
+    shutdown -n now
+    ;;
+  [nN]*)
+    echo "Exiting"
+    exit
+    ;;
+  *) echo "Please enter 'y' or 'n'!" ;;
   esac
 done
